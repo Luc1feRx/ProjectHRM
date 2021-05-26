4e5d531acbccc569e32c86de7d4bab891d134751
Tu <clgtqwe1@gmail.com>
2021-05-26 22:32:59 +0700

    Project

--- QLNS.sql
+++ QLNS.sql
@@ -29,14 +29,40 @@ VALUES
 	N'Tú'
 )
 
-SELECT * FROM tbUsers
-SELECT * FROM tbUsers WHERE Username = 'admin' AND Pass = 'asd123'
-DELETE FROM dbo.tbUsers WHERE Username = 'admin'
+CREATE TABLE tbTTNVCoBan(
+	MaBoPhan char(10) NOT NULL,
+	MaPhong char(10) NOT NULL,
+	MaNV nvarchar(50) PRIMARY KEY NOT NULL,
+	HoTen nvarchar(50),
+	MaLuong nvarchar(50),
+	NgaySinh DATE,
+	GioiTinh nvarchar(5),
+	TTHonNhan nvarchar(50),
+	CCCD nvarchar(50),
+	NoiCap nvarchar(50),
+	ChucVu nvarchar(50),
+	LoaiHD nvarchar(50),
+	ThoiGian nvarchar(10),
+	NgayKy DATE,
+	NgayHetHan DATE,
+	GhiChu nvarchar(MAX)
+)
 
-SELECT * FROM tbUsers WHERE Username = 'admin' AND Pass = 'asd123'
+CREATE TABLE tbTTCaNhan(
+	MaNV nvarchar(50) NOT NULL,
+	HoTen nvarchar(50),
+	NgaySinh DATE,
+	NguyenQuan nvarchar(50),
+	DCThuongChu nvarchar(100),
+	DCTamChu nvarchar(100),
+	SDT char(12),
+	DanToc nvarchar(30),
+	TonGiao nvarchar(20),
+	QuocTich nvarchar(20),
+	HocVan nvarchar(30),
+	GhiChu nvarchar(MAX)
+)
 
-SELECT * FROM tbUsers WHERE Username = 'user1'
-
-UPDATE tbUsers SET Pass = 'asd123' WHERE(Username = 'user1' AND Pass = '123')
-
-SELECT * FROM tbQuyen
+CREATE TABLE tbBoPhan(
+	MaBoPhan char(10) PRIMARY KEY NOT NULL,
+	TenBoPhan nchar(10) 
